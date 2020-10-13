47779d0970743b3a2c8f8c07f2ccb29b85bd387e
vitor-luis <vitorluisdossantosbmx@hotmail.com>
2020-10-13 17:28:28 -0300

    criação do listar todos e create integrantes

--- src/app/shared/services/integrantes.service.ts
+++ src/app/shared/services/integrantes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AsiloWebApi } from 'src/app/app.api';
+import { GPESWebApi } from 'src/app/app.api';
 import { Integrantes } from '../models/integrantes.model';
 
 @Injectable({
@@ -14,10 +14,11 @@ export class IntegrantesService {
   params = new HttpParams()
 
   getAllIntegrantesWithParams(modifier: string): Observable<HttpResponse<Integrantes[]>> {
-    return this.http.get<Integrantes[]>(`${AsiloWebApi}/${modifier}/integrantes/listar-todos`, { params: this.params, observe: 'response' })
+    return this.http.get<Integrantes[]>(`${GPESWebApi}/${modifier}/integrantes/listar-todos`, { params: this.params, observe: 'response' })
   }
 
-  postIntegrantes(integrante: Integrantes) {
-    return this.http.post<Integrantes>(`${AsiloWebApi}/authenticated/integrantes/criar`, integrante, { observe: 'response' })
+  postIntegrantes(form: Integrantes) {
+    console.log(form)
+    return this.http.post<Integrantes>(`${GPESWebApi}/authenticated/integrantes/criar`, form, { observe: 'response' })
   }
 }
