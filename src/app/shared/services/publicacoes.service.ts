1b0c4a59ba1ee6849c987cf000c9b193f2115500
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-13 02:57:13 -0300

    Renomeando transparencia.service para publicacoes.service e suas referências ao modelo de dados

--- src/app/shared/services/publicacoes.service.ts
+++ src/app/shared/services/publicacoes.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from "@angular/core"
 import { HttpClient, HttpParams, HttpResponse } from "@angular/common/http"
 import { Observable } from "rxjs"
 import { GPESWebApi } from "../../app.api"
-import { Transparencia } from "../models/transparencia.model"
+import { Publicacoes } from "../models/publicacoes.model"
 import { FileSnippet } from "src/app/web-components/common/file-uploader/FileSnippet.class"
 
 @Injectable()
-export class TransparenciaService {
+export class PublicacoesService {
 
     constructor(private http: HttpClient) { }
 
@@ -14,13 +14,13 @@ export class TransparenciaService {
     deleteParams = new HttpParams()
 
     /**Função que realiza a requisição do tipo GET ao endpoint “/modificador/transparencia/listar-todos”. O endpoint serve para o modificador público e privado. Ao chamar a função deve ser explicitado qual modificador será utilizado. A requisição pode receber parâmetros. */
-    getDocumentsWithParams(modifier: string): Observable<HttpResponse<Transparencia[]>> {
-        return this.http.get<Transparencia[]>(`${GPESWebApi}/${modifier}/transparencia/listar-todos`, { params: this.params, observe: 'response' })
+    getDocumentsWithParams(modifier: string): Observable<HttpResponse<Publicacoes[]>> {
+        return this.http.get<Publicacoes[]>(`${GPESWebApi}/${modifier}/transparencia/listar-todos`, { params: this.params, observe: 'response' })
     }
 
     /**Função que realiza a requisição do tipo GET ao endpoint “/authenticated/transparencia/listar-um/title”. A requisição possui um parâmetro obrigatório (title). */
-    getDocumentByTile(title: string): Observable<HttpResponse<Transparencia>> {
-        return this.http.get<Transparencia>(`${GPESWebApi}/authenticated/transparencia/listar-um/${title}`, { observe: 'response' })
+    getDocumentByTile(title: string): Observable<HttpResponse<Publicacoes>> {
+        return this.http.get<Publicacoes>(`${GPESWebApi}/authenticated/transparencia/listar-um/${title}`, { observe: 'response' })
     }
 
     /**Função que realiza a requisição do tipo POST ao endpoint “/authenticated/transparencia/criar”. A requisição não pode receber parâmetros e deve ser do tipo Form Data. */
@@ -34,8 +34,8 @@ export class TransparenciaService {
     }
 
     /**Função que realiza a requisição do tipo DELETE ao endpoint “/authenticated/transparencia/apagar”. A requisição pode receber parâmetros. */
-    deleteDocument(): Observable<HttpResponse<Transparencia>> {
-        return this.http.delete<Transparencia>(`${GPESWebApi}/authenticated/transparencia/apagar`, { params: this.deleteParams, observe: 'response' })
+    deleteDocument(): Observable<HttpResponse<Publicacoes>> {
+        return this.http.delete<Publicacoes>(`${GPESWebApi}/authenticated/transparencia/apagar`, { params: this.deleteParams, observe: 'response' })
     }
 
     /**Função que realiza a requisição do tipo PUT ao endpoint “/authenticated/transparencia/atualizar/title”. A requisição possui um parâmetro obrigatório (title) e deve ser do tipo Form Data. */
@@ -43,8 +43,8 @@ export class TransparenciaService {
         return this.http.put<FormData>(`${GPESWebApi}/authenticated/transparencia/atualizar/${title}`, formData, { observe: 'response' })
     }
 
-    getDataByTitle(title: string): Observable<HttpResponse<Transparencia>> {
-        return this.http.get<Transparencia>(`${GPESWebApi}/authenticated/transparencia/getdata/${title}`, { observe: 'response' })
+    getDataByTitle(title: string): Observable<HttpResponse<Publicacoes>> {
+        return this.http.get<Publicacoes>(`${GPESWebApi}/authenticated/transparencia/getdata/${title}`, { observe: 'response' })
     }
 
     getFilesByTitle(title: string): Observable<HttpResponse<FileSnippet>> {
