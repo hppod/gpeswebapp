26612a882f10bf57a45019da8b12d842afc375a9
vitor-luis <vitorluisdossantosbmx@hotmail.com>
2020-10-13 15:13:43 -0300

    listagem de integrantes

---
+++ src/app/admin-panel/integrantes/novo-integrante/novo-integrante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-integrante',
+  templateUrl: './novo-integrante.component.html',
+  styleUrls: ['./novo-integrante.component.css']
+})
+export class NovoIntegranteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
