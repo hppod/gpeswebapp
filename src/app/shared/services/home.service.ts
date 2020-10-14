b016f81b0cc4fbf8e122dce214111ba8a8a9fd4a
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-14 12:37:56 -0300

    Resolvendo os conflitos

--- src/app/shared/services/home.service.ts
+++ src/app/shared/services/home.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-<<<<<<< HEAD
 import { GPESWebApi } from 'src/app/app.api';
-=======
-import { AsiloWebApi } from 'src/app/app.api';
->>>>>>> 597ccddab98af9ea0f7456d0fa98c84107e80867
 import { Evento } from '../models/evento.model';
 
 @Injectable({
@@ -18,10 +14,6 @@ export class HomeService {
   params = new HttpParams()
 
   getNoticiasThreeResults(modifier: string, size: string): Observable<HttpResponse<Evento[]>> {
-<<<<<<< HEAD
     return this.http.get<Evento[]>(`${GPESWebApi}/${modifier}/noticia/listar-todos/${size}`, { params: this.params, observe: 'response' })
-=======
-    return this.http.get<Evento[]>(`${AsiloWebApi}/${modifier}/noticia/listar-todos/${size}`, { params: this.params, observe: 'response' })
->>>>>>> 597ccddab98af9ea0f7456d0fa98c84107e80867
   }
 }
