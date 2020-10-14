2ebe46621394362d9c754f5e0078ecb7976ff98e
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-14 01:27:40 -0300

    Realizando modificações necessárias para transformar novo-transparencia em novo-publicacoes e implementando o create e listar todos

--- src/app/admin-panel/publicacoes/novo-publicacoes/novo-publicacoes.component.ts
+++ src/app/admin-panel/publicacoes/novo-publicacoes/novo-publicacoes.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, HostListener } from "@angular/core"
+import { Component, OnInit, OnDestroy, HostListener } from "@angular/core"
 import { FormBuilder, FormGroup, Validators } from "@angular/forms"
 import { Router } from "@angular/router"
 import { Observable, Subscription } from "rxjs"
 import { ToastrService } from "ngx-toastr"
 import { BsModalService, BsModalRef, ModalOptions } from "ngx-bootstrap/modal"
 import { PublicacoesService } from "./../../../shared/services/publicacoes.service"
-import { TransparenciaValidator } from "./../../../shared/validations/transparencia.validator"
+import { PublicacoesValidator } from "./../../../shared/validations/publicacoes.validator"
 import { ModalDialogComponent } from "./../../../web-components/common/modals/modal-dialog/modal-dialog.component"
 import { ModalFileUploadComponent } from "./../../../web-components/common/modals/modal-file-upload/modal-file-upload.component"
 import { requiredFileType } from "./../../../shared/functions/upload-file-validator.function"
@@ -20,16 +20,16 @@ import { CategoryService } from "./../../../shared/services/categories.service"
 import { ModalCreateCategoryComponent } from "./../../../web-components/common/modals/modal-create-category/modal-create-category.component"
 
 @Component({
-  selector: 'app-novo-transparencia',
+  selector: 'app-novo-publicacoes',
   templateUrl: './novo-publicacoes.component.html',
   styleUrls: ['./novo-publicacoes.component.css']
 })
-export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate {
+export class NovoPublicacoesComponent implements OnInit, OnDestroy, ComponentCanDeactivate {
 
   private httpReq: Subscription
 
   modalRef: BsModalRef
-  _formTransparencia: FormGroup
+  _formPublicacoes: FormGroup
   progress = 0
   success = false
   maxChars = 500
@@ -40,6 +40,7 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
   selectOptionCategory: Category[] = new Array()
   option: string
   selectedCategory: string = null
+  total: Number = 0
 
   configModal: ModalOptions = {
     backdrop: 'static',
@@ -49,11 +50,10 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
   constructor(
     private _builder: FormBuilder,
     private _modal: BsModalService,
-    private transparenciaService: PublicacoesService,
+    private _service: PublicacoesService,
     private _router: Router,
     private _toastr: ToastrService,
-    private _unique: TransparenciaValidator,
-    private uploaderService: FileUploaderService,
+    private _unique: PublicacoesValidator,
     private categoryService: CategoryService
   ) { }
 
@@ -63,22 +63,34 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
     this.getCategories()
   }
 
+  ngOnDestroy() {
+    if (this.httpReq) {
+      this.httpReq.unsubscribe()
+    }
+  }
+
   /**Função ativada quando o usuário suja algum formulário e tenta sair da página sem ter salvo ele. Para sair da página ele deve confirmar a ação, evitando que ele perca dados digitados sem querer. */
   @HostListener('window:beforeunload')
   canDeactivate(): Observable<boolean> | boolean {
-    if (this._formTransparencia.dirty) {
+    if (this._formPublicacoes.dirty) {
       return false
     }
     return true
   }
 
+  getPublicacoesWithParams() {
+    this.httpReq = this._service.getPublicacoesWithParams('authenticated').subscribe(response => {
+      if (response.status == 200)
+        this.total = response.body['count']
+    })
+  }
+
   /**Função para inicializar o formulário */
   initForm() {
-    this._formTransparencia = this._builder.group({
+    this._formPublicacoes = this._builder.group({
       titulo: this._builder.control(null, [Validators.required], this._unique.checkUniqueTitulo()),
       descricao: this._builder.control(null, [Validators.required]),
-      categoria: this._builder.control("Selecione", [Validators.required]),
-      arquivo: this._builder.control(null, [Validators.required, requiredFileType('pdf')])
+      categoria: this._builder.control("Selecione", [Validators.required])
     })
   }
 
@@ -98,7 +110,7 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
     this.modalRef.content.action.subscribe((data: string) => {
       this.getCategories()
       this.selectedCategory = data
-      this._formTransparencia.controls['categoria'].setValue(this.selectedCategory)
+      this._formPublicacoes.controls['categoria'].setValue(this.selectedCategory)
     })
   }
 
@@ -108,43 +120,33 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
     this.modalRef = this._modal.show(ModalDialogComponent, { initialState })
     this.modalRef.content.action.subscribe((answer) => {
       if (answer) {
-        this._router.navigate(['/admin/transparencia'])
-        this._formTransparencia.reset()
+        this._router.navigate(['/admin/publicacoes'])
+        this._formPublicacoes.reset()
       }
     })
-  }
-
-  /**Função que define os arquivos do serviço no componente */
-  setFiles() {
-    this.Files = this.uploaderService.selectedFiles
-    this.setDocumentOnForm()
   }
 
   /**Função que define o arquivo no FormData da requisição */
   setDocumentOnForm() {
     this.File = this.Files[0].file
-    this._formTransparencia.controls['arquivo'].setValue(this.File)
-    this._formTransparencia.updateValueAndValidity()
+    this._formPublicacoes.controls['arquivo'].setValue(this.File)
+    this._formPublicacoes.updateValueAndValidity()
   }
 
   /**Função que adiciona novos documentos ao banco de dados. */
   addNewDocuments() {
     this.success = false
-    this.modalRef = this._modal.show(ModalFileUploadComponent)
-    this.transparenciaService.postDocuments(toFormData(this._formTransparencia.value)).pipe(
-      toResponseBody()
-    ).subscribe(res => {
-      this.success = true
-      this._formTransparencia.reset()
-      this.modalRef.hide()
-      this.showToastrSuccess('O documento foi adicionado com sucesso')
-      this._router.navigate(['/admin/transparencia'])
-    }, err => {
-      this._formTransparencia.reset()
-      this.modalRef.hide()
-      this.showToastrError('Houve um erro ao adicionar o documento. Tente novamente.')
-      this._router.navigate(['/admin/transparencia'])
-    })
+    this._service.postDocuments(this._formPublicacoes.value)
+      .subscribe(res => {
+        this.success = true
+        this._formPublicacoes.reset()
+        this.showToastrSuccess('O documento foi adicionado com sucesso')
+        this._router.navigate(['/admin/publicacoes'])
+      }, err => {
+        this._formPublicacoes.reset()
+        this.showToastrError('Houve um erro ao adicionar o documento. Tente novamente.')
+        this._router.navigate(['/admin/publicacoes'])
+      })
   }
 
   /**Função para exibir um toastr de sucesso. */
@@ -165,10 +167,10 @@ export class NovoPublicacoesComponent implements OnInit, ComponentCanDeactivate 
 
   /**Getters */
   /**Função que retorna o valor do input “titulo”. */
-  get titulo() { return this._formTransparencia.get('titulo') }
+  get titulo() { return this._formPublicacoes.get('titulo') }
   /**Função que retorna o valor do input “descricao”. */
-  get descricao() { return this._formTransparencia.get('descricao') }
+  get descricao() { return this._formPublicacoes.get('descricao') }
   /**Função que retorna o valor do input “categoria”. */
-  get categoria() { return this._formTransparencia.get('categoria') }
+  get categoria() { return this._formPublicacoes.get('categoria') }
 
 }
