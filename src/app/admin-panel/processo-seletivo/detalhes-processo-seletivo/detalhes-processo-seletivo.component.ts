4ebbb20bedd63786ecac8cbfaaaae57a3074cf1b
stella-celentano <stellacelentano99@gmail.com>
2020-10-20 09:59:15 -0300

    Criando componente para visualizar os detalhes de processo seletivo

---
+++ src/app/admin-panel/processo-seletivo/detalhes-processo-seletivo/detalhes-processo-seletivo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-processo-seletivo',
+  templateUrl: './detalhes-processo-seletivo.component.html',
+  styleUrls: ['./detalhes-processo-seletivo.component.css']
+})
+export class DetalhesProcessoSeletivoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/admin-panel/processo-seletivo/processo-seletivo.module.ts
+++ src/app/admin-panel/processo-seletivo/processo-seletivo.module.ts
@@ -14,17 +14,20 @@ import { DragulaModule } from 'ng2-dragula'
 
 import { TodosProcessoSeletivoComponent } from './todos-processo-seletivo/todos-processo-seletivo.component';
 import { CreateProcessoSeletivoComponent } from './create-processo-seletivo/create-processo-seletivo.component';
-import { PendingChangesGuard } from "src/app/shared/guards/pending-changes.guard"
+import { PendingChangesGuard } from "src/app/shared/guards/pending-changes.guard";
+import { DetalhesProcessoSeletivoComponent } from './detalhes-processo-seletivo/detalhes-processo-seletivo.component'
 
 const ROUTES: Routes = [
   { path: '', component: TodosProcessoSeletivoComponent },
-  { path: 'create', component: CreateProcessoSeletivoComponent, canDeactivate: [PendingChangesGuard] }
+  { path: 'create', component: CreateProcessoSeletivoComponent, canDeactivate: [PendingChangesGuard] },
+  { path: 'detalhes/:title', component: DetalhesProcessoSeletivoComponent, canDeactivate: [PendingChangesGuard] }
 ]
 
 @NgModule({
   declarations: [
     TodosProcessoSeletivoComponent,
-    CreateProcessoSeletivoComponent
+    CreateProcessoSeletivoComponent,
+    DetalhesProcessoSeletivoComponent
   ],
   exports: [
     TodosProcessoSeletivoComponent,
