82f68a1d9dd9681aee19f2c0ffcd92e004ed1192
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-26 18:09:48 -0300

    Ajustando a rota de eventos.

--- src/app/web-app/web-app.routing.ts
+++ src/app/web-app/web-app.routing.ts
@@ -7,7 +7,8 @@ export const WebAppRoutes: Routes = [
     {
         path: 'institucional', children: [
             { path: 'home', component: HomeComponent },
-            { path: 'noticias', loadChildren: './noticias/noticias-web.module#NoticiaWebModule' },
+            { path: 'gpes', loadChildren: './gpes/gpes-web.module#GpesWebModule' },
+            { path: 'eventos', loadChildren: './eventos/eventos-web.module#EventoWebModule' },
             { path: 'sobre', loadChildren: './sobre/sobre-web.module#SobreWebModule' },
             { path: 'publicacoes', loadChildren: './publicacoes/publicacoes-web.module#PublicacoesWebModule' },
             { path: 'processo-seletivo', loadChildren: './processo-seletivo/processo-seletivo-web.module#ProcessoSeletivoWebModule' },
