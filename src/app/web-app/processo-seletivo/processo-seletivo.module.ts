dace6150957b6e1c2cba4b969c87e33d6472b3ca
stella-celentano <stellacelentano99@gmail.com>
2020-10-26 14:45:59 -0300

    Criando o módulo e componente de processo seletivo institucional

---
+++ src/app/web-app/processo-seletivo/processo-seletivo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProcessoSeletivoComponent } from './processo-seletivo.component';
+
+
+
+@NgModule({
+  declarations: [ProcessoSeletivoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProcessoSeletivoModule { }
